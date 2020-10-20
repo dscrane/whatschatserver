5ba4cb071dd30eac3f99e38082b7557b18d64862
dscrane <daegancrane@gmail.com>
2020-10-20 09:31:58 -0400

    fixing mistakes

---
+++ src/public/precache-manifest.51803d1fd55eb129f3da6a23441a72e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034f1007a55e3d3fb3a9324d3a3dcd9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2009fbf878f95d4f5e84",
+    "url": "/static/css/main.5c003a52.chunk.css"
+  },
+  {
+    "revision": "4c596f7a3fbc32ad5760",
+    "url": "/static/js/2.ec579a60.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ec579a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2009fbf878f95d4f5e84",
+    "url": "/static/js/main.1cf7818e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
