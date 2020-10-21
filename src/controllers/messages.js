532de827d18d7b0a758f61544f2ab45a69c36469
dscrane <daegancrane@gmail.com>
2020-10-21 10:53:33 -0400

    chats are now visible on initial load

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -3,9 +3,15 @@ const Message = require('../models/message')
 
 const router = express.Router();
 
-router.get('/messages/fetch', async (req, res) => {
+router.get('/messages/fetch?', async (req, res) => {
   console.info('messages/fetch hit')
-  res.send('messages/fetch hit')
+  try {
+    const messages = await Message.find({chatroomId: req.query.chatroomId}).limit(50).sort({createdAt: 1});
+    res.set('Content-Security-Policy', 'default-src *')
+    res.send({ chatroomId: req.query.chatroomId, messages: messages });
+  } catch (e) {
+    console.error(e)
+  }
 })
 
 module.exports = router;
