e27c5e39db0f3e71a18052afff71bf6d3bf33b54
dscrane <daegancrane@gmail.com>
2020-10-22 12:16:05 -0400

    updates

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -5,10 +5,12 @@ const { User } = require("../models/user");
 const authenticate = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
+    console.log(token);
     if (!token) {
       throw new Error();
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
