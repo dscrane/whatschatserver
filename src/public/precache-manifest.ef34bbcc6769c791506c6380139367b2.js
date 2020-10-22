6b1a2c1e34b5ad5db51a664758c8f75ed6645379
dscrane <daegancrane@gmail.com>
2020-10-22 12:18:18 -0400

    updates

---
+++ src/public/precache-manifest.ef34bbcc6769c791506c6380139367b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7fb0777ac35b9e32907cde71ad38fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f78cf582e2f460698841",
+    "url": "/static/css/main.ee48d7f5.chunk.css"
+  },
+  {
+    "revision": "691b086dc81ce6ef0a25",
+    "url": "/static/js/2.48098249.chunk.js"
+  },
+  {
+    "revision": "f0b4452e65837d40526b5972208b6bd7",
+    "url": "/static/js/2.48098249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78cf582e2f460698841",
+    "url": "/static/js/main.af03f562.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
